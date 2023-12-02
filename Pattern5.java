a5b98579857472b524c0a91b2bf814ca62effea4
AnonymousManan <mananaggarwal2001@gmail.com>
2023-12-02 10:09:49 +0530

    Pattern 5 Modified Java

--- Pattern5.java
+++ Pattern5.java
@@ -2,7 +2,6 @@ public class Pattern5 {
     public static void main(String[] args) {
         numberPattern(5);
     }
-
     static void numberPattern(int number) {
         for (int i = 1; i <= number * 2; i++) {
             int finalNumber = i <= 5 ? i : number * 2 - i; // ternary operator is being used for checking the condition.
